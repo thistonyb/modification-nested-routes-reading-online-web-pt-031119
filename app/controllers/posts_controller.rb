260670d0801356e5ba8b4374300b8164ef304904
Tony Baker <this.tonybaker@gmail.com>
2019-08-13 20:26:19 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,28 +17,9 @@ class PostsController < ApplicationController
   end
 
   def new
-    @post = Post.new
+    if params[:author_id] && !Author.exists?(params[:author_id])
+      redirect_to authors_path, alert: "Author not found."
+    else
+      @post = Post.new(author_id: params[:author_id])
+    end
   end
-
-  def create
-    @post = Post.new(post_params)
-    @post.save
-    redirect_to post_path(@post)
-  end
-
-  def update
-    @post = Post.find(params[:id])
-    @post.update(params.require(:post))
-    redirect_to post_path(@post)
-  end
-
-  def edit
-    @post = Post.find(params[:id])
-  end
-
-  private
-
-  def post_params
-    params.require(:post).permit(:title, :description)
-  end
-end
